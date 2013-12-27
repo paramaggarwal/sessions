344e8f5e41c100ebd0e82cd586f06e57141acbf1
Param Aggarwal <paramaggarwal@gmail.com>
2013-12-27 10:49:57 +0530

    no routes abstraction

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-
+var express = require('express');
 var app = module.exports = express.createServer();
 
-// Configuration
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'your secret here' }));
+app.use(app.router);
 
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+
+app.get('/', function(req, res){
+  res.send('hello there mr');
 });
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', routes.index);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
