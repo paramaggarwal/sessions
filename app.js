cf4ebccdc9d28937d53c1c94bb46edc86e7835e8
Param Aggarwal <paramaggarwal@gmail.com>
2013-12-27 16:49:35 +0530

    replace default cookieParser

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 var connect = require('connect');
+var cookie = require('cookie');
 
 connect()
-  .use(connect.favicon())
-  .use(connect.cookieParser())
-  .use(connect.session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+  .use(function(req, res, next) {
+    var pairs = req.headers.cookie.split(/[;,] */);
+    req.cookies = {};
+    req.signedCookies = {};
+    
+    pairs.forEach(function(pair) {
+        var eq_idx = pair.indexOf('=')
+
+        // skip things that don't look like key=value
+        if (eq_idx < 0) {
+            return;
+        }
+
+        var key = pair.substr(0, eq_idx).trim()
+        var val = pair.substr(++eq_idx, pair.length).trim();
+
+        // quoted values
+        if ('"' == val[0]) {
+            val = val.slice(1, -1);
+        }
+
+        // only assign once
+        if (undefined == req.cookies[key]) {
+            try {
+                req.cookies[key] = decodeURIComponent(val);
+            } catch (e) {
+                req.cookies[key] = val;
+            }
+        }
+    });
+
+    next();
+  })
+  .use(connect.session({ secret: 'secret' }))
   .use(function(req, res, next){
     var sess = req.session;
     if (sess.views) {
