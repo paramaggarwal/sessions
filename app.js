3b0b5386d972e569bd2fef988f602e1b0d71fc36
Param Aggarwal <paramaggarwal@gmail.com>
2013-12-27 10:48:16 +0530

    remove templating and public assets

--- app.js
+++ app.js
@@ -11,14 +11,11 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
